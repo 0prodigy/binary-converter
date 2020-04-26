13170eadd993f41eb4cd0dd8224ecb716fd0b24f
Akash <pathakvikash9211@gmail.com>
2020-04-26 20:20:49 +0530

    decimal to binary coverter

---
+++ main.js
@@ -0,0 +1,30 @@
+let arr = [];
+function converter(num) {
+  //   let arr = [];
+  if (num <= 2) {
+    arr.unshift(1, 0);
+    return;
+  }
+  let remainder = num % 2;
+  arr.unshift(remainder);
+  if (num % 2 == 0) {
+    num = num / 2;
+  } else {
+    num = (num - 1) / 2;
+  }
+
+  converter(num);
+}
+
+// console.log(arr.join(""));
+let decimal = document.getElementById("decimal");
+let btn = document.getElementById("covert");
+let result = document.getElementById("result");
+
+function clicked() {
+  //   console.log(decimal.innerHTML);
+  //   console.log(decimal);
+  decimal = parseInt(decimal.value);
+  converter(decimal);
+  result.value = arr.join("");
+}
